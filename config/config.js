442c3ac9e04bd3640bf5165a9775e733d68bbef2
Daniel Koza <dako@MacBook-Pro-Daniel.local>
2015-06-16 19:06:12 +0200

    heroku deploy

--- config/config.js
+++ config/config.js
@@ -1,9 +1,9 @@
 angular.module("huBEERt.config", [])
 
 .constant("ENVIR", {
-	"ENV_NAME": "http://192.168.0.10:3000/api/v1/",
-	"BACKEND_URL": "http://192.168.0.10:3000",
-	"API_URL": "http://192.168.0.10:3000/api/v1/"
+	"ENV_NAME": "http://192.168.0.24:3000/api/v1/",
+	"BACKEND_URL": "http://192.168.0.24:3000",
+	"API_URL": "http://192.168.0.24:3000/api/v1/"
 })
 
 ;
